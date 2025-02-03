124ea78602add2e33f6aae089329d2f8e16d3a11
Elisa <elisac02@outlook.com>
2025-02-03 16:35:07 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+   public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override String ToString()
+        {
+            return Nome
+                + " , $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + " , "
+                + Quantidade
+                + " unidades, Total $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
