5a463c87ca3e70fcb32a3229d9c58d36da4e4445
Elisa venica <140559340+Elisavenica@users.noreply.github.com>
2025-02-04 15:51:50 -0300

    Update Produto.cs

--- Course/Produto.cs
+++ Course/Produto.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+using System.Globalization;
 
 namespace Course
 {
@@ -11,6 +11,14 @@ namespace Course
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
         }
         public override String ToString()
         {
@@ -26,3 +34,6 @@ namespace Course
         }
     }
 }
+
+
+        
